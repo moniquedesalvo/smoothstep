b16360f4713e741c77a60b7484656833bd31b34e
moniquedesalvo <moniquerenee@gmail.com>
2017-11-22 14:22:13 -0800

    update swipe.js

--- scripts/swipe.js
+++ scripts/swipe.js
@@ -1,22 +1,36 @@
 //swipe
-var swipeLeft = new Hammer(overlayRightEl);
-var swipeRight = new Hammer(overlayLeftEl);
+var swipeOverlayRight = new Hammer(overlayRightEl);
+var swipeOverlayLeft = new Hammer(overlayLeftEl);
 
 
-swipeLeft.get('swipe').set({direction: Hammer.DIRECTION_ALL});
+swipeOverlayLeft.get('swipe').set({direction: Hammer.DIRECTION_ALL});
 
-swipeRight.get('swipe').set({direction: Hammer.DIRECTION_ALL});
+swipeOverlayRight.get('swipe').set({direction: Hammer.DIRECTION_ALL});
 
 
-swipeLeft.on("swipeleft", function () {
+swipeOverlayLeft.on("swipeleft", function () {
 	if (lightboxEl.style.display === "block") {
 		goToNextImg();
 	}
 });
 
-swipeRight.on("swiperight", function () {
+swipeOverlayLeft.on("swiperight", function () {
 	if (lightboxEl.style.display === "block") {
 		goToPreviousImg();
 	}
 });
+
+swipeOverlayRight.on("swipeleft", function () {
+	if (lightboxEl.style.display === "block") {
+		goToNextImg();
+	}
+});
+
+swipeOverlayRight.on("swiperight", function () {
+	if (lightboxEl.style.display === "block") {
+		goToPreviousImg();
+	}
+});
+
+
 
